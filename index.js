c88079ca367d60d3fb72613aae03ba81fd1fe3e9
Bitti09 <bitti06@gmail.com>
2018-10-21 19:03:57 +0200

    Last Update for today

--- index.js
+++ index.js
@@ -3,22 +3,31 @@ const { ApolloServer, gql } = require("apollo-server-express");
 const { importSchema } = require("graphql-import");
 const typeDefs = importSchema("schema.graphql");
 const resolvers = require("./resolvers.js");
+const { ApolloEngine } = require("apollo-engine");
 
 // Construct a  schema, using GraphQL schema language
 // Provide resolver functions for your schema fields
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  tracing: true,
+  cacheControl: true,
+  // We set `engine` to false, so that the new agent is not used.
+  engine: false
 });
 
 const app = express();
 server.applyMiddleware({
   app
 });
-app.listen(
+const engine = new ApolloEngine({
+  apiKey: "key"
+});
+engine.listen(
   {
-    port: 4000
+    port: 4000,
+  expressApp: app
   },
   () =>
     console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
